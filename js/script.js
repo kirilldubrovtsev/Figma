3c6f10cb56f2bee13648e7a99c35a5f32078d111
kirill dubrovtsev <dubrovtsev.kirill@gmail.com>
2023-02-23 12:57:36 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 let button = document.querySelector(".button");
 let loginpopup = document.querySelector(".login_popup");
-
+let popupclose = document.querySelector(".popup_close")
 
 button.addEventListener('click', ShowPopup);
 
+
+
 function ShowPopup(evt) {
+  console.log(evt);
+  evt.preventDefault();
 
-console.log(evt);
-evt.preventDefault();  
-
-loginpopup.classList.toggle ('show-popup')
-
+  loginpopup.classList.toggle('show-popup')
 
 };
